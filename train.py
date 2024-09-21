afa77750fba307a88aa631ae6371af1cfee7d501
Rensdebee <R.35rens@gmail.com>
2024-09-21 19:58:56 +0200

    added test script + moved shared functions to utils file

--- train.py
+++ train.py
@@ -1,9 +1,5 @@
 import argparse
-from datasets import load_dataset, Dataset
-import random
-import torch
-import numpy as np
-import pandas as pd
+from datasets import load_dataset
 from transformers import (
     AutoModelForSequenceClassification,
     AutoTokenizer,
@@ -11,63 +7,11 @@ from transformers import (
     Trainer,
     TrainingArguments,
 )
-import evaluate
-from scipy.special import softmax
 
-
-def seed(seed):
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-
-
-def convert_dataset(dataset, bot):
-    ds = []
-    for row in dataset:
-        q = row["question"]
-        human = row["human_answers"]
-        model = row[f"{bot}_answers"]
-        for h in human:
-            ds.append([q, h, 0])
-        for m in model:
-            ds.append([q, m, 1])
-
-    ds = Dataset.from_pandas(pd.DataFrame(ds, columns=["question", "answer", "label"]))
-
-    return ds
-
-
-def compute_metrics(eval_preds):
-    metrics = {}
-    logits, labels = eval_preds
-    predictions = np.argmax(logits, axis=-1)
-    probs = softmax(logits, axis=-1)
-
-    roc_auc = evaluate.load("roc_auc")
-    precision = evaluate.load("precision")
-    recall = evaluate.load("recall")
-    f1 = evaluate.load("f1")
-    accuracy = evaluate.load("accuracy")
-
-    metrics["accuracy"] = accuracy.compute(predictions=predictions, references=labels)[
-        "accuracy"
-    ]
-    metrics["precision"] = precision.compute(
-        predictions=predictions, references=labels
-    )["precision"]
-    metrics["recall"] = recall.compute(predictions=predictions, references=labels)[
-        "recall"
-    ]
-    metrics["f1"] = f1.compute(predictions=predictions, references=labels)["f1"]
-    metrics["roc_auc"] = roc_auc.compute(
-        prediction_scores=probs[np.arange(len(labels)), labels], references=labels
-    )["roc_auc"]
-    return metrics
-
+from utils import compute_metrics, seed, convert_dataset
+import json
 
 def get_datasets(train_domain, test_domain, train_model, test_model, ratio=0.2):
-
     # Filter dataset based on the source
     ds = load_dataset("Hello-SimpleAI/HC3", "reddit_eli5", split=train_domain)
     ds = convert_dataset(ds, train_model)
@@ -123,7 +67,8 @@ def main(args):
         per_device_eval_batch_size=args.batch_size,
         eval_strategy="steps",
         eval_steps=1000,
-        save_strategy="epoch",
+        save_strategy="steps",
+        save_steps=1000
     )
 
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
@@ -138,8 +83,10 @@ def main(args):
     )
 
     trainer.train()
-    trainer.eval_dataset(test_dataset)
-
+    test_results = trainer.evaluate(test_dataset)
+    print(test_results, "w")
+    with open(output_dir+"/test_results.json") as f:
+        json.dump(test_results, f)
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser("OOD AI detector trainer")
@@ -175,7 +122,7 @@ if __name__ == "__main__":
         "-testm",
         "--test_model",
         type=str,
-        help="Split used for training",
+        help="Split used for testing",
         default="chatgpt",
     )
     args.add_argument("-b", "--batch-size", type=int, default=16, help="batch size")
